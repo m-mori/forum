69065c8df1734ea5cd3c30dc443edf24ccbc9b85
m-mori <mori.mayumi@sat.ne.jp>
2016-01-20 16:58:31 +0900

    添付ファイル機能をプラグイン追加、日本語化対応

---
+++ addons/languages/Japanese/definitions.Attachments.php
@@ -0,0 +1,8 @@
+<?php
+// Japanese Definitions for the Attachments plugin.
+
+$definitions["Attach a file"] = "画像ファイル";
+$definitions["Drop files to upload"] = "ドロップして画像追加";
+$definitions["Embed in post"] = "画像挿入";
+$definitions["message.attachmentNotFound"] = "いずれかの理由のため、画像ファイルを表示できません。存在しない、または表示するアクセス許可がない可能性があります。";
+
