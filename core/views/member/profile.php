d7e3bcb67e750c6882bcc175869294ee96087a4f
m-mori <mori.mayumi@sat.ne.jp>
2016-01-21 16:30:52 +0900

    [プロフ] delete部分の修正対応

--- core/views/member/profile.php
+++ core/views/member/profile.php
@@ -22,41 +22,12 @@ $member = $data["member"];
 <h1 id='memberName'><?php echo name($member["username"]); ?></h1>
 
 <?php
-// Online indicator.
-if (empty($member["preferences"]["hideOnline"])):
-	$lastAction = ET::memberModel()->getLastActionInfo($member["lastActionTime"], $member["lastActionDetail"]);
-	if ($lastAction) echo "<".(!empty($lastAction[1]) ? "a href='{$lastAction[1]}'" : "span")." class='online' title='".T("Online").($lastAction[0] ? " (".sanitizeHTML($lastAction[0]).")" : "")."'><i class='icon-circle'></i></".(!empty($lastAction[1]) ? "a" : "span").">";
-endif;
-?>
-
-<?php
 // Output the email if the viewer is an admin.
 if (ET::$session->isAdmin()): ?><p class='subText'><?php echo sanitizeHTML($member["email"]); ?></p><?php endif; ?>
 
-<p id='memberGroup' class='subText'><?php echo memberGroup($member["account"], $member["groups"], true); ?></p>
-<p id='memberLastActive' class='subText'><?php printf(T("Last active %s"), empty($member["preferences"]["hideOnline"])
-	? "<span title='".date(T("date.full"), $member["lastActionTime"])."'>".relativeTime($member["lastActionTime"], true)."</span>"
-	: "[".T("hidden")."]"); ?></p>
-
 </div>
 
-<?php
-// Output the member actions menu.
-if ($data["actions"]->count()): ?>
-<ul id='memberActions'>
-<?php echo $data["actions"]->getContents(); ?>
-</ul>
-<?php endif; ?>
-
 </div>
-
-<?php
-// Output the member controls menu.
-if ($data["controls"]->count()): ?>
-<ul id='memberControls' class='controls'>
-<?php echo $data["controls"]->getContents(); ?>
-</ul>
-<?php endif; ?>
 
 <ul id='memberPanes' class='tabs big'>
 <?php echo $data["panes"]->getContents(); ?>
