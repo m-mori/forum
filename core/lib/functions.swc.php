0d51e94542b87ce20264ae308600ee041d56eb86
m-mori <mori.mayumi@sat.ne.jp>
2016-02-05 16:12:14 +0900

    [共通] SWCのリンク先url を取得するメソッド追加変更

--- core/lib/functions.swc.php
+++ core/lib/functions.swc.php
@@ -3,8 +3,22 @@
 
 if (!defined("IN_ESOTALK")) exit;
 
+// SWC definitions ---------------------------------//
+if (!defined("SWC_MEMBERS")) define("SWC_MEMBERS", '//members.swc.dev/');    // TODO: domain name
+if (!defined("SWC_MEMBERS_LOGIN")) define("SWC_MEMBERS_LOGIN", SWC_MEMBERS .'login/');    // TODO: domain name
+if (!defined("SWC_MEMBERS_ENTRY")) define("SWC_MEMBERS_ENTRY", SWC_MEMBERS .'entry/');    // TODO: domain name
+
 //const FMT_TIME = 'Y年m月d日 H時i分';
 const FMT_TIME = 'Y/m/d H:i';
+
+function getSwcUrl($name) {
+    if ($name=='login') {
+        return SWC_MEMBERS_LOGIN;
+    } else if ($name=='entry') {
+        return SWC_MEMBERS_ENTRY;
+    }
+    return "";
+}
 
 function getStrfTime($time=null, $fmt=null) {
     $t = $time ? $time : time();
@@ -12,3 +26,4 @@ function getStrfTime($time=null, $fmt=null) {
     $str = date($fmt, $t);
     return $str;
 }
+
