af2512581c3bab86d2283c67e2f6af6c21507ea0
m-mori <mori.mayumi@sat.ne.jp>
2016-02-10 18:00:02 +0900

    [設定ファイル] ローカルのデフォルト登録

---
+++ config/config.php
@@ -0,0 +1,18 @@
+<?php
+$config["esoTalk.installed"] = true;
+$config["esoTalk.version"] = '1.0.0g4';
+$config["esoTalk.database.host"] = 'localhost';
+$config["esoTalk.database.user"] = 'subaru';
+$config["esoTalk.database.password"] = 'impreza';
+$config["esoTalk.database.dbName"] = 'new_subaru_community';
+$config["esoTalk.database.prefix"] = 'ft_';
+$config["esoTalk.forumTitle"] = 'SWCフォーラム';
+$config["esoTalk.baseURL"] = 'http://members.swc.dev/forum/';
+$config["esoTalk.emailFrom"] = 'do_not_reply@members.swc.dev';
+$config["esoTalk.cookie.name"] = 'SWC';
+$config["esoTalk.urls.friendly"] = true;
+$config["esoTalk.urls.rewrite"] = true;
+$config["skin.Default.primaryColor"] = '#364159';
+
+// Last updated by: satおいしい水 (192.168.33.1) @ Fri, 05 Feb 2016 19:13:27 +0900
+?>
