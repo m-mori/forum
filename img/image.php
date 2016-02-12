f778467ba356b983d0ae8a2effb644518d66292b
m-mori <mori.mayumi@sat.ne.jp>
2016-02-12 11:03:32 +0900

    [共通] SWCプロフィール画像取得IF 追加対応

---
+++ img/image.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * SWCからユーザプロフィール画像取得
+ */
+include_once '../define.php';
+include_once(SWC_PROJECT_PATH .'app/Subarucommunity_Controller.php');
+Subarucommunity_Controller::main('Subarucommunity_Controller', 'forum_image');
+?>
