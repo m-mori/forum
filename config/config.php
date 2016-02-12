0db963254a299838b5920021e55c5d753a140a97
m-mori <mori.mayumi@sat.ne.jp>
2016-02-12 10:57:28 +0900

    [設定] ローカル設定を更新

--- config/config.php
+++ config/config.php
@@ -7,12 +7,12 @@ $config["esoTalk.database.password"] = 'impreza';
 $config["esoTalk.database.dbName"] = 'new_subaru_community';
 $config["esoTalk.database.prefix"] = 'ft_';
 $config["esoTalk.forumTitle"] = 'SWCフォーラム';
-$config["esoTalk.baseURL"] = 'http://members.swc.dev/forum/';
+//$config["esoTalk.baseURL"] = 'https://members.subaru.dev/forum/';
+$config["esoTalk.baseURL"] = 'https://members.swc.dev/forum/';
+//$config["esoTalk.emailFrom"] = 'do_not_reply@members.subaru.dev';
 $config["esoTalk.emailFrom"] = 'do_not_reply@members.swc.dev';
 $config["esoTalk.cookie.name"] = 'SWC';
 $config["esoTalk.urls.friendly"] = true;
 $config["esoTalk.urls.rewrite"] = true;
 $config["skin.Default.primaryColor"] = '#364159';
-
-// Last updated by: satおいしい水 (192.168.33.1) @ Fri, 05 Feb 2016 19:13:27 +0900
 ?>
--- define.php
+++ define.php
@@ -1,3 +1,4 @@
 <?php
 // SWC 設定
 define('SWC_PROJECT_PATH', '/hostdev/swc/osaka_var_opt/ethna/subarucommunity/');
+//define('SWC_PROJECT_PATH', '/var/opt/ethna/subarucommunity/');
