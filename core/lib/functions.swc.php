fa634a0d6390b8ce7e4f872ef47855689b0f93c3
m-mori <mori.mayumi@sat.ne.jp>
2016-02-15 10:05:00 +0900

    [設定] SWCリソース設定をdefine.php へ定義

--- core/lib/functions.swc.php
+++ core/lib/functions.swc.php
@@ -3,22 +3,8 @@
 
 if (!defined("IN_ESOTALK")) exit;
 
-// SWC definitions ---------------------------------//
-if (!defined("SWC_MEMBERS")) define("SWC_MEMBERS", '//members.swc.dev/');    // TODO: domain name
-//if (!defined("SWC_MEMBERS")) define("SWC_MEMBERS", '//members.subaru.dev/');    // TODO: domain name
-if (!defined("SWC_MEMBERS_LOGIN")) define("SWC_MEMBERS_LOGIN", SWC_MEMBERS .'login/');    // TODO: domain name
-if (!defined("SWC_MEMBERS_ENTRY")) define("SWC_MEMBERS_ENTRY", SWC_MEMBERS .'entry/');    // TODO: domain name
-
-// TODO: SNSボタン
-if (!defined("SWC_TW_BTN")) define("SWC_TW_BTN", 1);        // Twitter ボタン
-if (!defined("SWC_FB_BTN")) define("SWC_FB_BTN", 1);        // Fb ボタン
-if (!defined("SWC_LIKE_BTN")) define("SWC_LIKE_BTN", 1);    // SWCいいね ボタン
-
 //const FMT_TIME = 'Y年m月d日 H時i分';
 const FMT_TIME = 'Y/m/d H:i';
-
-// -------------------------------------------------//
-
 
 /**
  * SWC側へのリンクurl取得
--- define.php
+++ define.php
@@ -1,4 +1,19 @@
 <?php
-// SWC 設定
+// SWC ethna/subarucommunity パス設定
 define('SWC_PROJECT_PATH', '/hostdev/swc/osaka_var_opt/ethna/subarucommunity/');
 //define('SWC_PROJECT_PATH', '/var/opt/ethna/subarucommunity/');
+// TODO: SWC ドメイン名 ※プロトコルリラティブで設定
+define("SWC_MEMBERS", '//members.swc.dev/');
+//define("SWC_MEMBERS", '//members.subaru.dev/');
+
+// ログインurl
+define("SWC_MEMBERS_LOGIN", SWC_MEMBERS .'login/');
+// 新規登録url
+define("SWC_MEMBERS_ENTRY", SWC_MEMBERS .'entry/');
+
+// TODO: SNSボタン設定
+define("SWC_TW_BTN", 1);        // Twitter ボタン
+define("SWC_FB_BTN", 1);        // Fb ボタン
+define("SWC_LIKE_BTN", 1);      // SWCいいね ボタン
+
+
