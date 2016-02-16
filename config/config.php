15d1276703bbf3437cd55c7e9ed2481d068b1070
m-mori <mori.mayumi@sat.ne.jp>
2016-02-16 14:06:50 +0900

    [設定ファイル] configファイルの更新

--- config/config.php
+++ config/config.php
@@ -7,12 +7,11 @@ $config["esoTalk.database.password"] = 'impreza';
 $config["esoTalk.database.dbName"] = 'new_subaru_community';
 $config["esoTalk.database.prefix"] = 'ft_';
 $config["esoTalk.forumTitle"] = 'SWCフォーラム';
-//$config["esoTalk.baseURL"] = 'https://members.subaru.dev/forum/';
 $config["esoTalk.baseURL"] = 'https://members.swc.dev/forum/';
-//$config["esoTalk.emailFrom"] = 'do_not_reply@members.subaru.dev';
 $config["esoTalk.emailFrom"] = 'do_not_reply@members.swc.dev';
 $config["esoTalk.cookie.name"] = 'SWC';
 $config["esoTalk.urls.friendly"] = true;
 $config["esoTalk.urls.rewrite"] = true;
 $config["skin.Default.primaryColor"] = '#364159';
+$config["esoTalk.admin.welcomeShown"] = false;
 ?>
