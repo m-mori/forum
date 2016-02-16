14dfda415d53daecea067550dc781c39d34d35c5
m-mori <mori.mayumi@sat.ne.jp>
2016-02-16 18:29:45 +0900

    [設定ファイル] configファイルの更新（添付画像設定追加）

--- config/config.php
+++ config/config.php
@@ -13,5 +13,29 @@ $config["esoTalk.cookie.name"] = 'SWC';
 $config["esoTalk.urls.friendly"] = true;
 $config["esoTalk.urls.rewrite"] = true;
 $config["skin.Default.primaryColor"] = '#364159';
-$config["esoTalk.admin.welcomeShown"] = false;
+$config["esoTalk.admin.welcomeShown"] = true;
+$config["esoTalk.language"] = 'Japanese';
+$config["esoTalk.forumLogo"] = false;
+$config["esoTalk.defaultRoute"] = 'conversations';
+$config["esoTalk.visibleToGuests"] = '1';
+$config["esoTalk.members.visibleToGuests"] = false;
+$config["esoTalk.registration.open"] = '1';
+$config["esoTalk.registration.requireConfirmation"] = 'email';
+$config["esoTalk.conversation.editPostTimeLimit"] = 300;
+$config["Attachments.version"] = '1.0.0g4';
+$config["esoTalk.enabledPlugins"] = array (
+  0 => 'Attachments',
+  1 => 'BBCode',
+  2 => 'Emoticons',
+);
+$config["plugin.Attachments.allowedFileTypes"] = array (
+  0 => 'jpg',
+  1 => 'jpeg',
+  2 => 'gif',
+  3 => 'png',
+  4 => 'bmp',
+);
+$config["plugin.Attachments.maxFileSize"] = '26214400';
+
+// Last updated by: satおいしい水 (192.168.10.157) @ Tue, 16 Feb 2016 18:21:52 +0900
 ?>
