24a659066cfb0026eaefbaeb4606d08b2ae34a4b
m-mori <mori.mayumi@sat.ne.jp>
2016-02-24 12:03:18 +0900

    [共通] define 設定。画像関連情報を追加

--- define.php
+++ define.php
@@ -16,4 +16,16 @@ define("SWC_TW_BTN", 1);        // Twitter ボタン
 define("SWC_FB_BTN", 1);        // Fb ボタン
 define("SWC_LIKE_BTN", 1);      // SWCいいね ボタン
 
+// 画像サイズ
+define("SWC_IMG_MAX_H", 1024);  // MAX値 縦
+define("SWC_IMG_MAX_W", 1024);  // MAX値 横
+// サムネイル画像サイズ
+define("SWC_IMG_THUMB_H", 150);  // サムネイル 縦
+define("SWC_IMG_THUMB_W", 200);  // サムネイル 横
+
+// 一覧メイン画像サムネイル表示設定 1: 表示
+define("SWC_MAIN_THUMB_DISPLAY", 1);
+
+// デフォルト画像 サムネイルパス
+define("SWC_NOIMG_THUMB_PATH", "img/noimage.jpg");
 
