04200d426ae706afb8e46750288c3f04a49488d3
m-mori <mori.mayumi@sat.ne.jp>
2016-03-01 17:06:47 +0900

    [プラグイン] ブックマーク機能プラグインを追加。日本語化対応済み

---
+++ addons/languages/English/definitions.Bookmarks.php
@@ -0,0 +1,7 @@
+<?php
+// Japanese Definitions for the Bookmarks plugin.
+
+$definitions["Bookmark"] = "Bookmark";
+$definitions["Unbookmark"] = "Unbookmark";
+$definitions["gambit.bookmarked"] = "bookmarked";
+$definitions["label.bookmarked"] = "Bookmarked";
---
+++ addons/languages/Japanese/definitions.Bookmarks.php
@@ -0,0 +1,7 @@
+<?php
+// Japanese Definitions for the Bookmarks plugin.
+
+$definitions["Bookmark"] = "ブックマーク";
+$definitions["Unbookmark"] = "ブックマーク解除";
+$definitions["gambit.bookmarked"] = "ブックマーク済み";
+$definitions["label.bookmarked"] = "ブックマーク済み";
